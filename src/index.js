fb5458569dc02ee3c7426284fb1a06aa92626089
jimztech <ibezimjames20@gmail.com>
2025-05-01 20:31:28 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const openMenu = document.getElementById("hamburger-menu");
 const navBar = document.getElementById("nav-links");
+const main = document.getElementById("main-content");
 
-openMenu.addEventListener("click", () => {
+
+function toggleMenu() {
+    if(window.innerWidth < 900) {
+        if(navBar.style.display === "flex") {
+            navBar.style.display = "none";
+            openMenu.src = "./images/icon-menu.svg";
+        } else {
+            navBar.style.display = "flex";
+            openMenu.src = "./images/icon-close.svg";
+        }
+    }
+}
+openMenu.addEventListener("click", toggleMenu);
+main.addEventListener("click", () => {
     if(navBar.style.display === "flex") {
         navBar.style.display = "none";
         openMenu.src = "./images/icon-menu.svg";
-    } else {
-        navBar.style.display = "flex";
-        openMenu.src = "./images/icon-close.svg";
     }
 });
